8a289a7f362cadcf7360aa4658a69738e3d08711
jsa0224 <voidness45@gmail.com>
2023-01-06 15:51:50 +0900

    feat: NetworkManager, NetworkError 타입 구현

---
+++ Diary/Resource/Network/NetworkError.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkError.swift
+//  Diary
+//
+//  Created by SummerCat and som on 2023/01/06.
+//
+
+enum NetworkError: Error {
+    case clientError
+    case missingData
+    case serverError
+}
---
+++ Diary/Resource/Network/NetworkManager.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkManager.swift
+//  Diary
+//
+//  Created by SummerCat and som on 2023/01/06.
+//
+
+import Foundation
+
+struct NetworkManager {
+    public static let publicNetworkManager = NetworkManager()
+    
+    func getJSONData<T: Codable>(url: String, type: T.Type, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        HTTPManager.shared.requestGet(url: url) { result in
+            switch result {
+            case .success(let data):
+                guard let data: T = JSONConverter.shared.decodeData(data: data) else {
+                    completion(.failure(.missingData))
+                    return
+                }
+                completion(.success(data))
+            case .failure(_):
+                return
+            }
+        }
+    }
+}
