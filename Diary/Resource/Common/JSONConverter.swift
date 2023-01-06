5dd85a4aa4286fc838527fddb91e93adbaadf717
jsa0224 <voidness45@gmail.com>
2023-01-06 15:47:08 +0900

    feat: JSONConverter 타입 구현

---
+++ Diary/Resource/Common/JSONConverter.swift
@@ -0,0 +1,24 @@
+//
+//  JSONConverter.swift
+//  Diary
+//
+//  Created by 정선아 on 2023/01/06.
+//
+
+import Foundation
+
+final class JSONConverter {
+    static let shared = JSONConverter()
+    
+    func decodeData<T: Codable>(data: Data) -> T? {
+        do {
+            let result = try JSONDecoder().decode(T.self, from: data)
+            return result
+        } catch DecodingError.dataCorrupted(let context) {
+            print(context.codingPath, context.debugDescription, context.underlyingError ?? "", separator: "\n")
+            return nil
+        } catch {
+            return nil
+        }
+    }
+}
