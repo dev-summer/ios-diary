b8d96e1b07757381b895f7e53e00c7c648e6ba55
jsa0224 <voidness45@gmail.com>
2023-01-06 15:46:45 +0900

    feat: HTTPMethod 타입 구현

---
+++ Diary/Resource/Network/HTTPMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPMethod.swift
+//  Diary
+//
+//  Created by SummerCat and som on 2023/01/06.
+//
+
+enum HTTPMethod {
+    static let get: String = "GET"
+    static let post: String = "POST"
+    static let patch: String = "PATCH"
+    static let delete: String = "DELETE"
+}
