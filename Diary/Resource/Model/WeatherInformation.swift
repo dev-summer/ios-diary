267e649859dbb64b3ca35a59bd5e477de27367f0
jsa0224 <voidness45@gmail.com>
2023-01-06 16:32:19 +0900

    feat: JSON 데이터를 넣을 WeatherInformation 타입 구현

---
+++ Diary/Resource/Model/WeatherInformation.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherInformation.swift
+//  Diary
+//
+//  Created by SummerCat and som on 2023/01/06.
+//
+
+import Foundation
+
+struct WeatherInformation: Decodable {
+    let weather: Weather
+}
+
+struct Weather: Decodable {
+    let icon: String
+}
