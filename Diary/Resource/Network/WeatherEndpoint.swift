79d034a93daea4a28dade00721e21d1c7f4afc74
jsa0224 <voidness45@gmail.com>
2023-01-07 16:07:02 +0900

    fix: 아이콘 이미지 URL과 api URL의 baseURL 분리하여 구현

--- Diary/Resource/Network/WeatherEndpoint.swift
+++ Diary/Resource/Network/WeatherEndpoint.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum WeatherEndpoint {
-    case fetchWeatherInformation(latitude: Double, longitude: Double, appid: String = "b87014a68c343a8a60a32e16da67aa98")
+    case fetchWeatherInformation(latitude: Double, longitude: Double)
     case fetchWeatherIconImage(icon: String)
 }
 
@@ -23,7 +23,16 @@ extension WeatherEndpoint {
     }
     
     var baseURL: String {
-        return "https://api.openweathermap.org"
+        switch self {
+        case .fetchWeatherInformation(let latitude, let longitude):
+            return "https://api.openweathermap.org"
+        case .fetchWeatherIconImage(let icon):
+            return "http://openweathermap.org"
+        }
+    }
+    
+    var appid: String {
+        return "b87014a68c343a8a60a32e16da67aa98"
     }
     
     var path: String {
@@ -35,16 +44,16 @@ extension WeatherEndpoint {
         }
     }
     
-    var queries: [URLQueryItem] {
+    var queries: [URLQueryItem]? {
         switch self {
-        case .fetchWeatherInformation(let latitude, let longitude, let appid):
+        case .fetchWeatherInformation(let latitude, let longitude):
             var queryParameters: [URLQueryItem] = []
             queryParameters.append(URLQueryItem(name: "lat", value: "\(latitude)"))
             queryParameters.append(URLQueryItem(name: "lon", value: "\(longitude)"))
             queryParameters.append(URLQueryItem(name: "appid", value: "\(appid)"))
             return queryParameters
         default:
-            return []
+            return nil
         }
     }
     
